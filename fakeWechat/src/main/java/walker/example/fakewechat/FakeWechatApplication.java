96096a41860d19a807498989b8fb4361bc7a8757
yachao.ni <yachao.ni@ipinyou.com>
2022-03-15 13:57:04 +0800

    netty新增

---
+++ fakeWechat/src/main/java/walker/example/fakewechat/FakeWechatApplication.java
@@ -0,0 +1,13 @@
+package walker.example.fakewechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeWechatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FakeWechatApplication.class, args);
+    }
+
+}
---
+++ fakeWechat/src/test/java/walker/example/fakewechat/FakeWechatApplicationTests.java
@@ -0,0 +1,13 @@
+package walker.example.fakewechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeWechatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
